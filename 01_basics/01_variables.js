0e53d92685bd60f1a9c5fb1904bccec8e666eb89
Ganesh Dhonde <154231934+ganeshdhonde2001@users.noreply.github.com>
2026-07-26 14:16:12 +0000

    learned what are variables , declaration and initialization , also learned about multiline  string

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 2233
+let accountEmail = 'user@gmail.com'
+var accountPassword = '12345'
+address = 'Latur'
+
+
+accountEmail = 'user2@gmail.com'
+accountPassword = '54321'
+address = 'sindhgaon'
+let accountState ;
+/* dont use var because of issue in block scope and function scope*/ 
+
+// accountId = 3344  occurs error: TypeError: Assignment to constant variable. NOT ALLOWED
+console.table([accountId,accountEmail,accountPassword,address,accountState])
+
